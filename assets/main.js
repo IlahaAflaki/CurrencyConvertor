2db0698a3db5c4c12c2b66f69602ef4659cea282
IlahaAflaki <aflaki.ilaha@gmail.com>
2022-05-02 14:56:28 +0400

    added two sided convertion

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,9 @@
 let btnsLeft = document.querySelectorAll('.currency_elements_left');
 let btnsRight = document.querySelectorAll('.currency_elements_right');
-let inpt = document.querySelector('input');
+let inpt = document.querySelector('.input_currency');
 let baseCurrency = 'RUB', targetCurrency = 'USD';
 let amount = 1;
-let money_target = document.querySelector('.money');
+let inptRight = document.querySelector('.money');
 let money_txt = document.querySelectorAll('.money-txt');
 
 
@@ -15,10 +15,17 @@ inpt.addEventListener('input', () => {
 
 });
 
+inptRight.addEventListener('input', () => {
+
+
+    getValue1(baseCurrency, targetCurrency, inptRight.value);
+
+});
+
+
 btnsLeft.forEach(button => {
     button.addEventListener('click', function () {
         baseCurrency = button.innerText
-        console.log(baseCurrency)
         btnsLeft.forEach(oldButton => {
             oldButton.classList.remove('active');
         });
@@ -26,7 +33,8 @@ btnsLeft.forEach(button => {
         this.classList.add('active');
 
 
-        getValue(baseCurrency, targetCurrency, inpt.value);
+        // getValue(baseCurrency, targetCurrency, inpt.value);
+        getValue1(baseCurrency, targetCurrency, inptRight.value)
     });
 });
 
@@ -55,7 +63,7 @@ function getValue(baseCurrency, targetCurrency, amount) {
 
     request.onload = function () {
         let response = request.response;
-        console.log(response);
+        // console.log(response);
 
 
         money_txt[0].innerText = `1 ${baseCurrency} = ${response.result} ${targetCurrency}`
@@ -63,10 +71,38 @@ function getValue(baseCurrency, targetCurrency, amount) {
 
 
         if (Number(inpt.value) === 0) {
-            money_target.innerText = ''
+            inptRight.value = ''
         } else {
-            money_target.innerText = amount * response.result
+            
+            inptRight.value = amount * response.result;
         }
+
+    }
+
+}
+
+
+function getValue1(baseCurrency, targetCurrency, amount) {
+
+    let requestURL = `https://api.exchangerate.host/convert?from=${targetCurrency}&to=${baseCurrency}`;
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+        let response = request.response;
+        console.log(response);
+
+
+        money_txt[0].innerText = `1 ${baseCurrency} = ${response.result} ${targetCurrency}`
+        money_txt[1].innerText = `1 ${targetCurrency} = ${1 / response.result} ${baseCurrency}`
+
+            console.log(amount)
+
+            inpt.value = amount * response.result;
+        
+
     }
 
 }
